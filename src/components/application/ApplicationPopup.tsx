93b097795c3e33c12e4e4b9b10d72f1e9058faa3
Theoneste NDAYISENGA <ndayisengatheoneste79@gmail.com>
2024-12-26 20:32:10 +0200

    adding new line of usa certificate

--- src/components/application/ApplicationPopup.tsx
+++ src/components/application/ApplicationPopup.tsx
@@ -46,6 +46,7 @@ const ApplicationPopup = () => {
               <li>• Flexible learning/Payment options</li>
               <li>• Industry-recognized certification</li>
               <li>• Discount of 40% (130,000 Rwf)</li>
+              <li>• Earn USA Certificate in Data Analytics</li>
               <li>• More Discount For Students, Disable and Female</li>
             </ul>
           </div>
