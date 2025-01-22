2a1cf7f2979857a5f1f6fa524dd6ea1b7710a83a
Theoneste NDAYISENGA <ndayisengatheoneste79@gmail.com>
2025-01-22 14:19:16 +0200

    Add and Change the call for application

--- src/components/hero/ApplicationButtons.tsx
+++ src/components/hero/ApplicationButtons.tsx
@@ -5,7 +5,7 @@ const ApplicationButtons = () => {
   return (
     <div className="flex flex-col sm:flex-row gap-4 justify-center">
       <a
-        href="https://forms.gle/ZjAYrMH3V4gbbP5q9"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSdwfSmDVERtXUEXuiNV7YMstg_lNEH7QJYEKvSLdk1RYmVrJg/viewform"
         className="inline-flex items-center justify-center px-8 py-4 text-lg font-medium rounded-lg text-white bg-green-600 hover:bg-green-700 transform transition-all hover:scale-105 shadow-lg hover:shadow-green-500/25"
       >
         <UserPlus className="w-6 h-6 mr-2" />
